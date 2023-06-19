63bd382352368cc0dbc58cf9c2783763cb09b0de
akms-limon <akmslimon@gmail.com>
2023-06-19 17:08:55 +0600

    "Laptop"

---
+++ Neumerical/jordan.cpp
@@ -0,0 +1,12 @@
+/**
+ *    author: A K M S Limon
+ *    created: 19-June-2023  17:07:00
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout <<"limon"<<endl;
+      
+    return 0;
+}
