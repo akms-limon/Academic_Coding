6c6d79bf6384f7f3c0fcb157293694f00a5d0377
akms-limon <akmslimon@gmail.com>
2023-06-19 16:59:14 +0600

    added from laptop

---
+++ Neumerical/jack.cpp
@@ -0,0 +1,12 @@
+/**
+ *    author: A K M S Limon
+ *    created: 19-June-2023  16:58:06
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Hello world\n";
+      
+    return 0;
+}
